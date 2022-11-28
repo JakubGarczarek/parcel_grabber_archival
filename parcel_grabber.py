787c28d6f118d10190dd59cd935bd48988921d5c
Jakub Garczarek <jakub.garczarek@gmail.com>
2022-11-28 19:48:51 +0100

    klasa Uldk w oddzielnym pliku, a główny skrypt  w parcel_grabber.py

--- parcel_grabber.py
+++ parcel_grabber.py
@@ -1,35 +1,7 @@
-from wfs import get_wfs, gml_to_wkt
-from uldk import uldk_to_csv, uldk_to_postgis, bbox_from_csv
-from postgis import connection, wkt_to_postgis
+from uldk import Uldk
 
-url = 'https://geoportal.powiat.kielce.pl/map/geoportal/wfs.php' 
-service = 'WFS'
-request = 'GetFeature'
-version = '2.0.0'
-typenames = 'ewns:dzialki'
-bbox = '324922,607897,326156,611727'
-srsname = 'EPSG:2180'
+csv = 'testowy.csv'
 
-
-
-
-csv = 'radko.csv'
-metoda = 'GetParcelById'
-#metoda = 'GetAggregateArea'
-format = 'geom_wkt'
-# format = 'geom_extend'
-output = 'radko.wkt'
-
-# uldk_to_csv(csv, metoda, format, output)
-
-
-
-# bazaTest = connection('użytkownik', 'hasło', 'ip', 'port', 'nazwa_bazy')git 
-# uldk_to_postgis(csv, metoda,format,bazaTest,'plots')
-# bbox = bbox_from_csv(csv,metoda,format,'bbox.txt')
-# print(bbox)
-# wkt_to_postgis(bazaTest,'plots','bbox',bbox)
-# get_wfs(url, service, request, version, typenames, bbox, srsname)
-
-plik_gml = 'radko.gml'
-gml_to_wkt(plik_gml)
+uldk = Uldk(csv)
+uldk.json()
+uldk.bbox()
--- uldk.py
+++ uldk.py
@@ -1,98 +1,106 @@
-import requests, re
-from postgis import wkt_to_postgis
+import requests, re, json
 
-class Bbox():
+class Uldk():
 
-    """Wyznaczanie bboxa dla podanej listy terytów działek"""
+    def __init__(self, csv):
+        self.csv = csv
 
-    def __init__(self, csv, output): #wymaga podania źródła terytów (csv) i do jakiego pliku zapisać niepowodzenia(output)
-        self.csv = csv #źródło teyrtów (np. baza gruntów w csv)
-        self.output = output #do jakiego pliku zapisać niepowodzenia
-        self.metoda = 'GetParcelById' #parametr do zapytania ULDK, może być też 'GetAggregateArea'
-        self.format = 'geom_wkt' #parametr do zapytania ULDK,domyślnie jest geom_wkb, może być też geom_extend
+    ###################################################
+    # CSV (lista TERYTÓW) => JSON {"TERYT":"GEOM_WKT"}
+    ###################################################
 
-    def bbox_from_csv(self): #przyjmuje csv z listą terytów, zwraca xy narożników bboxa w stringu pasującego do zapytania WFS (pion, poziom)
-        compare_list = [] #utworzenie pustej listy porównawczej, do której później wpadać będą pary kompletów (listy) współrzędnych
-        with open(self.csv) as f:
-            for line in f: #iteracja przez wszystkie linie pliku csv z terytami
-                teryt = line.strip() #pobranie terytu z oczyszczonej linii pliku csv
-                querry= f"https://uldk.gugik.gov.pl/?request={self.metoda}&id={teryt}&result={self.format}" #taki jest format zapytania http ULDK
-                response = requests.get(querry) #użycie biblioteki request do wykonania ww. zapytania
-                if response.status_code == 200: #zabezpieczenie przed błędami połączenia
-                    xy = re.search('POLYGON\(\((.+?)\)\)', str(response.content)).group(1)#wydobycie samych współrzędnych z contentu zapytania
-                    geometria = f"POLYGON(({xy}))" #opakowanie współrzędnych formatem wkt
-                    to_compare = self.min_max_xy(geometria) #wrzucenie do funkcji zwracającej LISTĘ min i max współrzędnych przygotowany wyżej wkt
-                    compare_list.append(to_compare) #dodanie ww. LISTY min i max współrzędnych do przygotowanej wcześniej listy porównawczej
-                    if len(compare_list)==2: #działanie gdy aktualnie w liście porównawczej znajdują się 2 komplety (listy) wspórzędnych
-                        latest = self.compare_xy(compare_list[0], compare_list[1]) #wysyłka 2 list z ekstremami, zwrotka stringa z minX, minY, maxX, maxY i do zmiennej
-                        compare_list=[latest] #redukcja zawartości listy porównawczej z 2 podlist do 1 listy zawierającej ww. min i max wsp z tych 2 pierwotnych list
-                        print(compare_list) #żeby nie pomyśleć że skrypt wisi ;)
-                else: #działania gdyby serwer z uldk nie odpowiedział
-                    print(f" adres {response} zwrócił kod {response.status_code}")
-                    with open(f"braki_{self.output}", "a",encoding = 'utf-8') as f:
-                        f.write(teryt+"\n") #zapisanie do pliku terytów bez odpowiedzi
-                    print(f"zapisuję {teryt} do braki_{self.output}") #info o powyższym dla usera
-        bbox = f"{compare_list[0][1]},{compare_list[0][0]},{compare_list[0][3]},{compare_list[0][2]}" #min Y, min X, max Y, max X w stringu do zmiennej,
-        return bbox #którą finalnie zwrócimy
-
-    def min_max_xy(self, geom): #potrzebna tylko do bbox_from_csv(), zwraca listę min max współrzędnych z dostarczonego wkt
-        xy_list = re.findall("\d+\.\d+",geom) #wydobycie samych współrzędnych z dostarczonego wkt i do listy
-        x_list = xy_list[0::2] #wydobycie samych x z powyższych współrzędnych i do listy
-        y_list = xy_list[1::2] #wydobycie samych y z powyższczych współrzędnych i do listy
-        min_x = min(x_list) # wydobycie najmniejszych x z powyższych współrzędnych i do zmiennej
-        min_y = min(y_list) # wydobycie najmniejszych y z powyższych współrzędnych i do zmiennej
-        max_x = max(x_list) # wydobycie największych x z powyższych współrzędnych i do zmiennej
-        max_y = max(y_list) # wydobycie największych y z powyższych współrzędnych i do zmiennej
-        return [min_x, min_y, max_x, max_y] #zwrot powyższych min i max w postaci listy
+    def json(self):
+        # narazie pusty słownik {"TERYT":"GEOM_WKT"}
+        teryt_geom = {}
+        # otwieramy csv z terytami
+        with open(self.csv) as csv:
+            for line in csv:
+                # pobranie pojedynczego terytu z csv
+                teryt = line.strip()
+                # zapytanie do usługi uldk z podaniem terytu
+                querry= f"https://uldk.gugik.gov.pl/?request=GetParcelById&id={teryt}&result=geom_wkt"
+                response = requests.get(querry)
+                # gdy serwer odpowie poprawnie i coś zwróci w content
+                if response.status_code == 200:
+                    # serwer zwraca wkt z białymi znakami
+                    wkt_uncleaned = str(response.content)
+                    # czyścimy 
+                    # wyciągnięcie samych współrzędnych
+                    only_xy = re.search('POLYGON\(\((.+?)\)\)', wkt_uncleaned).group(1) 
+                    # ponowne opakowanie ich w POLYGON(()) wg formatu wkt
+                    geom = f"POLYGON(({only_xy}))"
+                    # dodanie do słownika pojedynczej pary {"TERYT":"GEOM_WKT"}
+                    teryt_geom[teryt] = geom
+                # gdy brak odpowiedzi z serwera lub content pusty
+                else:
+                    # utwórz plik o nazwie jak csv ale z prefixem "braki" (nadpisanie)
+                   with open(f"braki_{self.csv}", "w",encoding = 'utf-8') as f:
+                    # zapisz w nowej lini teryt którego geometrii serwer nie zwrócił
+                        f.write(teryt+"\n")  
+                    
+        # zapis słownika {"TERYT":"GEOM_WKT"} do pliku JSON (nadpisanie)
+        with open (f"{self.csv}.json", "w", encoding='utf-8') as f:
+            json.dump(teryt_geom, f)
+        # dodatkowo zwrotka finalnego wyniku (nie tylko json)
+        return teryt_geom
     
-    def compare_xy(self, minmax1, minmax2): #potrzebna tylko do bbox_from_csv(), przyjmuje 2 listy min i max współrzędnych, zwraca ich min i max współrzędne luzem
-        smaller_x = min(minmax1[0], minmax2[0]) # min z samych najmniejszych x'ów
-        smaller_y = min(minmax1[1], minmax2[1]) # min z samych najmniejszych y'ków
-        bigger_x = max(minmax1[2], minmax2[2])  # max z samych największych x'ów
-        bigger_y = max(minmax1[3], minmax2[3]) # max z samych największych y'ków
-        return(smaller_x, smaller_y, bigger_x, bigger_y) #zwrot
+    
+    ####################################
+    # JSON {"TERYT":"GEOM_WKT"} => BBOX
+    ####################################
 
-    def save_to_file(self, file): # zapisuje bboxa zwróconego przez bbox_from_csv do dowolnego pliku
-        with open (file, "a", encoding='utf-8') as f: #otwarcie pliku w którym zapisany zostanie bbox
-            f.write(self.bbox_from_csv()) #zapis bboxa do podanego pliku (uruchamia całą metodę bbox_from_csv)
-
-
-def uldk_to_csv(csv, metoda, format, output):
-    with open(csv) as f:
-        for line in f:
-            teryt=line.strip()
-            querry= f"https://uldk.gugik.gov.pl/?request={metoda}&id={teryt}&result={format}"
-            response = requests.get(querry)
-            if response.status_code == 200:
-                xy = re.search('POLYGON\(\((.+?)\)\)', str(response.content)).group(1)
-                geometria = f"POLYGON(({xy}))"
-                with open(f"{output}", "a",encoding = 'utf-8') as f:
-                    f.write(geometria+"\n")
-                print(geometria)
-            else:
-                print(f" adres {response} zwrócił kod {response.status_code}")
-                with open(f"braki_{output}", "a",encoding = 'utf-8') as f:
-                    f.write(teryt+"\n")
-                print(f"zapisuję {teryt} do braki_{output}")
-
-
-def uldk_to_postgis(csv, metoda, format, connection, tab):
-    with open(csv) as f:
-        for line in f:
-            teryt=line.strip()
-            querry= f"https://uldk.gugik.gov.pl/?request={metoda}&id={teryt}&result={format}"
-            response = requests.get(querry)
-            if response.status_code == 200:
-                xy = re.search('POLYGON\(\((.+?)\)\)', str(response.content)).group(1)
-                geometria = f"POLYGON(({xy}))"
-                wkt_to_postgis(connection, tab, teryt, geometria)
-                # with open(f"{output}", "a",encoding = 'utf-8') as f:
-                #     f.write(geometria+"\n")
-                print(geometria)
-            else:
-                print(f" adres {response} zwrócił kod {response.status_code}")
-                with open(f"braki_{connection}", "a",encoding = 'utf-8') as f:
-                    f.write(teryt+"\n")
-                print(f"zapisuję {teryt} do braki_{connection}")
-
-
+    def bbox(self):
+        #utworzenie pustej listy porównawczej, 
+        # do której później wpadać będą pary kompletów (listy) współrzędnych
+        compare_list = []
+        #otworzenie jsona stworzonego przez uldk.json()
+        with open(f"{self.csv}.json") as f:
+            j = json.load(f)
+            # iteracja przez wszystkie pary {"TERYT":"GEOM_WKT"} z jsona
+            for geom in j.values():
+                # wyodrębnienie samych liczb (współrzędnych) z geom (do listy)
+                xy_list = re.findall("\d+\.\d+",geom)
+                # lista samych x-ów (co 2 element od 0)
+                x_list = xy_list[0::2]
+                # lista samych y-ków (co 2 element od 1)
+                y_list = xy_list[1::2]
+                # dodanie ich extremów do utworzonej wcześniej listy porównawczej
+                compare_list.append( [min(x_list), min(y_list), max(x_list), max(y_list)] )
+                # Jeżeli w tej liście znajduje się aktualnie
+                # komplet (para) list współrzędnych do porównania
+                # tworzymy z nich jedną listę z ekstremami 
+                if len(compare_list) == 2:
+                    # pobranie kompletów ekstremów do zmiennych a i b
+                    # a oraz b to listy o strukturze [min x, min y, max x, max y]
+                    a = compare_list[0]
+                    b = compare_list[1]
+                    # najmniejszy x min (pomiędzy a i b)
+                    ab_min_x = min(a[0], b[0])
+                    # najmniejszy y min (pomiędzy a i b)
+                    ab_min_y = min(a[1], b[1])
+                    # największy x max (pomiędzy a i b)
+                    ab_max_x = max(a[2], b[2])
+                    # największy y max (pomiędzy a i b)
+                    ab_max_y = max(a[3], b[3])
+                    # zastąpienie a i b w compare_list 
+                    # na jedną listę zawierającą ekstrema 
+                    # z porównania a i b
+                    compare_list = [[ab_min_x, ab_min_y, ab_max_x, ab_max_y]]
+                    
+                # jeśli compare_list zawiera tylko 1 element pomijamy redukcję a i b 
+                # (bo jest tylko nowe a) i wracamy do początku pętli w celu dodania 
+                # kolejnego b    
+        # finalnie compare_list zawiera jedną zwycięzką listę [0]
+        # z której wyciągniemy wsp bboxa
+        x_min = compare_list[0][0]
+        y_min = compare_list[0][1]
+        x_max = compare_list[0][2]
+        y_max = compare_list[0][3]
+        # usługa wfs potrzebuje stringa z odwróconymi wspołrzędnymi
+        # oddzielonymi przecinkami
+        bbox = f"{y_min},{x_min},{y_max},{x_max}"
+        # zapis bboxa do pliku
+        with open (f"bbox_{self.csv}", 'w', encoding='utf-8') as f:
+            f.write(bbox)
+        return bbox
+        
